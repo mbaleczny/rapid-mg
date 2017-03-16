b7a3383703c5b45c8157871ab07dfb4c3f1d32e9
mbaleczny <mariuszbaleczny@gmail.com>
2017-03-16 17:30:27 +0100

    Fix ExampleInstrumentedTest

--- app/src/androidTest/kotlin/pl/mbaleczny/rapid_mg/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/pl/mbaleczny/rapid_mg/ExampleInstrumentedTest.kt
@@ -1,10 +1,13 @@
 package pl.mbaleczny.rapid_mg
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import pl.mbaleczny.rapid_mg.tweetList.ui.TweetListActivity
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -13,12 +16,17 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField val rule: ActivityTestRule<TweetListActivity>
+            = ActivityTestRule(TweetListActivity::class.java)
+
     @Test
     @Throws(Exception::class)
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-
-        assertEquals("pl.mbaleczny.rapid_mg", appContext.packageName)
+        val flavor: String = if (BuildConfig.FLAVOR === "prod") "" else "." + BuildConfig.FLAVOR
+        assertEquals("pl.mbaleczny.rapid_mg" + flavor, appContext.packageName)
     }
 }
